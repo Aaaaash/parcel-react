b49ec2c098e6ef72860c7386330750747a9800fa
sakuraash <841105127@qq.com>
2017-12-06 22:30:02 +0800

    add mypage

---
+++ MyPage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class MyPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        this is my page;
+      </div>
+    );
+  }
+}
+
+export default MyPage;
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from 'styled-components';
+
+import MyPage from './MyPage';
+
+const Container = styled.div`
+  display: flex;
+`;
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return <Container>
+      {this.props.name}
+      <MyPage />
+    </Container>;
   }
 }
 
